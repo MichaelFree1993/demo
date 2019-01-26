bd8e345557a0c6221d46708bfa8500e7bb2ae17e
10243385 <ma.gao@zte.com.cn>
2019-01-26 16:46:38 +0800

    ThreadLocalDemo案例

--- src/concurrent/ThreadLocalDemo.java
+++ src/concurrent/ThreadLocalDemo.java
@@ -1,6 +1,21 @@
 package concurrent;
 
 public class ThreadLocalDemo {
+    private static final ThreadLocal threadLocal = new ThreadLocal();
 
-    ThreadLocal threadLocal = new ThreadLocal();
+    public static void main(String[] args) throws Exception{
+        Thread thread1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                threadLocal.set("thread");
+                System.out.println(threadLocal.get());
+            }
+        });
+        thread1.start();
+        thread1.join();
+
+        threadLocal.set("main");
+        System.out.println(threadLocal.get());
+    }
 }
+
