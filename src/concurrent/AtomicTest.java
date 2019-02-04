2d3a14fd0192a6e6a5977992e3847ec8cdcf8d88
MichealFree1993 <magaofree@163.com>
2019-02-05 06:56:52 +0800

    增加原子操作demo

--- src/concurrent/AtomicTest.java
+++ src/concurrent/AtomicTest.java
@@ -1,4 +1,49 @@
 package concurrent;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class AtomicTest {
+    private int i = 0;
+    private AtomicInteger atomicInteger = new AtomicInteger();
+
+    public static void main(String[] args) {
+        AtomicTest atomicTest = new AtomicTest();
+        List<Thread> threadList = new ArrayList<>();
+        for (int j = 0; j < 100; j++) {
+            Thread thread = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    for(int i = 0 ; i<1000;i++){
+                        atomicTest.unsafeCount();
+                        atomicTest.safeCount();
+                    }
+                }
+            });
+            threadList.add(thread);
+            thread.start();
+        }
+
+        for(Thread thread : threadList){
+            try{
+                thread.join();
+            }catch (Exception ex){
+                ex.printStackTrace();
+            }
+        }
+        System.out.println(atomicTest.i);
+        System.out.println(atomicTest.atomicInteger.get());
+    }
+
+
+    //同步计数
+    public void safeCount() {
+        atomicInteger.getAndIncrement();
+    }
+
+    //非同步计数
+    public void unsafeCount() {
+        i++;
+    }
 }
---
+++ src/concurrent/MyLock.java
@@ -0,0 +1,71 @@
+package concurrent;
+
+import java.util.HashMap;
+import java.util.concurrent.locks.Condition;
+
+public class MyLock {
+
+    //内部同步器
+    public static class Sync {
+        private volatile int state = 0;
+        private Thread exclusiveOwnerThread;
+        private Condition condition;
+
+        public int getState() {
+            return state;
+        }
+
+        public void setState(int state) {
+            this.state = state;
+        }
+
+        public Thread getExclusiveOwnerThread() {
+            return exclusiveOwnerThread;
+        }
+
+        public void setExclusiveOwnerThread(Thread exclusiveOwnerThread) {
+            this.exclusiveOwnerThread = exclusiveOwnerThread;
+        }
+
+        public boolean tryLock(int num) {
+            if (compareAndSetState(0, 1)) {
+                setExclusiveOwnerThread(Thread.currentThread());
+                return true;
+            }
+            return false;
+        }
+
+        public boolean releaseLock(int num) throws Exception {
+            if (getState() == 0) {
+                throw new Exception("state is wrong");
+            }
+            int i = getState() - num;
+            setState(i);
+            if (i == 0) {
+                setExclusiveOwnerThread(null);
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        public Condition getCondition(){
+            return condition;
+        }
+    }
+
+    //新建同步队列器对象
+    Sync sync = new Sync();
+
+    public boolean tryLock(int num) {
+        return sync.tryLock(num);
+    }
+
+    public boolean releaseLock(int num) throws Exception {
+        return sync.releaseLock(num);
+    }
+
+    private boolean compareAndSetState() {
+
+    }
+}
